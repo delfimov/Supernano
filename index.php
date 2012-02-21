091ed72a2e0010e38348e970a9f044f4568cb727
Groozly <elfimov@gmail.com>
2012-02-21 16:38:36 +0400

    bugfix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /*
  * supernano framework 
  *
- * Copyright (c) 2011 by Dmitry Elfimov
+ * Copyright (c) 2012 by Dmitry Elfimov
  * Released under the MIT License.
  * http://www.opensource.org/licenses/mit-license.php
  *
@@ -11,7 +11,8 @@
  */
 
 $_d = dirname(__FILE__).'/tpl/';
-$_t = $_d.((isset($_GET['c']) && !empty($_GET['c'])) ? preg_replace('/[^a-zA-Z0-9_]/', '_', $_GET['c']) : 'index').'.php';
+$_c = isset($_GET['c']) ? trim($_GET['c'], "/\/\\ \t\n\r\0\x0B") : '';
+$_t = $_d.($_c!='' ? preg_replace('/[^a-zA-Z0-9_-]/', '_', $_c) : 'index').'.php';
 if (!file_exists($_t)) {
 	header("HTTP/1.0 404 Not Found");
 	$_t = $_d.'error404.tpl.php';
