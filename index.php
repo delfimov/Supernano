84e70b4513d73ece922b993685e802f09497e28e
Groozly <elfimov@gmail.com>
2012-04-05 18:28:56 +0400

    v1.0.0

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author    Dmitry Elfimov <elfimov@gmail.com>
  * @copyright 2012 Dmitry Elfimov
  * @license   http://www.elfimov.ru/nanobanano/license.txt MIT License
- * @version   GIT: <git_id>
+ * @version   GIT: v1.0.0
  * @link      http://github.com/Groozly/Supernano
  *
  */
